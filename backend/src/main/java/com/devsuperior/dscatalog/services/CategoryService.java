fc810e3cbd1e369f0d8e2707ef40f1768e474655
Thiago Barraviera <thiagobarraviera@gmail.com>
2025-05-26 20:02:03 -0300

    feat: add transactional annotation

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +16,9 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    // Anotação que indica que as operações são somente leitura no banco. Isso ajuda o Spring a otimizar o desempenho e a evitar bloqueios desnecessários.
+    // Usado em metodos que apenas leem dados findAll(), findById()...
+    @Transactional(readOnly = true)
     // Metodo que busca todas as categorias
     public List<Category> findAll() {
         // esse .findAll() ja é um metodo de busca pronto do Jpa, é nós utilizarmos
