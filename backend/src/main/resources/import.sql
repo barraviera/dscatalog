bbcc68dee3bd3bd8165f79b2dde9fc1b9fbd11a5
Thiago Barraviera <thiagobarraviera@gmail.com>
2025-05-26 20:02:14 -0300

    feat: add seed

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+// Arquivo de seed para alimentar o banco com dados para teste
+INSERT INTO tb_category(name) VALUES ('Books');
+INSERT INTO tb_category(name) VALUES ('Eletronics');
+INSERT INTO tb_category(name) VALUES ('Computers');
