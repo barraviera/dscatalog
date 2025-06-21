166ebb0dee5b095e2a297ee76096a74854ecabaa
Thiago Barraviera <thiagobarraviera@gmail.com>
2025-06-21 08:40:00 -0300

    feat: product entity sql script

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -0,0 +1,127 @@
+package com.devsuperior.dscatalog.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    // Atributos
+
+    // Anotação pra informar qual atributo será o id
+    @Id
+    // Anotação pra deixar o id auto incrementado
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private Double price;
+    private String imgUrl;
+
+    // Ao trabalhar com data no banco, usar esta anotação pro banco armazenar no formato UTC
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant date;
+
+    // Anotação pra indicar que o relacionamento será de N:N muitos pra muitos
+    // ou seja 1 produto pode ter varias categorias e 1 categoria pode ter varios produtos
+    @ManyToMany
+    // Anotação pra indicar a tabela intermediaria que chamaremos de tb_product_category
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    // Usaremos o Set para não deixar repetir dados
+    Set<Category> categories = new HashSet<>();
+
+    // Contrutor sem argumentos
+
+    public Product() {
+    }
+
+    // Construtor com argumentos
+
+    public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+        this.date = date;
+    }
+
+    // Getter e Setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    // Hashcode e Equals
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
