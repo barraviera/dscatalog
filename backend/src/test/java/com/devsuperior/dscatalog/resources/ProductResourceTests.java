460f7bf12d3afac357e73ac7070349eea8829cb3
Thiago Barraviera <thiagobarraviera@gmail.com>
2025-06-29 10:19:36 -0300

    feat: test findById method for when id exists and when id does not exist

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.services.ProductService;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscatalog.tests.Factory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,30 +18,35 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // Testes na camada web, ou seja, controller, ou resources como podemos chamar tambem
 @WebMvcTest(ProductResource.class)
 public class ProductResourceTests {
 
-    // Atributos
+    // ATRIBUTOS ---
 
     @Autowired
     private MockMvc mockMvc;
-
     // Usar o @MockBean ao inves do @Mock, quando a classe de teste carrega o contexto da aplicação
     // e precisa mockar bean do sistema. Ex. quando usar o @WebMvcTest, usaremos o @MockBean
     @MockBean
     private ProductService service;
-
     // Usar o PageImpl pra instanciar um objeto concreto
     private PageImpl<ProductDTO> page;
-
     private ProductDTO productDTO;
+    private Long existingId;
+    private Long nonExistingId;
+
+    // SIMULADOR ---
 
     // Simuladores, que executa antes de cada metodo de teste
     @BeforeEach
     void setUp() throws Exception {
+
+        existingId = 1L;
+        nonExistingId = 2L;
 
         // Usamos a classe Factory que criamos para nos auxiliar
         productDTO = Factory.createProductDTO();
@@ -50,9 +56,15 @@ public class ProductResourceTests {
         // Quando chamarmos o metodo findAllPaged do ProductService passando qualquer argumento any()
         // ele terá que retornar um objeto page do tipo PageImpl<ProductDTO>
         when(service.findAllPaged(any())).thenReturn(page);
+
+        // Simular o comportamento do findById do ProductResource
+        // Quando chamarmos o findById do ProductService passando um id existente ele deve retornar um ProductDTO
+        when(service.findById(existingId)).thenReturn(productDTO);
+        // Quando chamarmos o findById do ProductService passando um id inexistente ele deve lançar a excessao ResourceNotFoundException
+        when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
     }
 
-    // Metodos
+    // METODOS/TESTES ---
 
     @Test
     // o findAllPaged do ProductResource deveria retornar um page
@@ -63,6 +75,34 @@ public class ProductResourceTests {
                 .accept(MediaType.APPLICATION_JSON));
 
         result.andExpect(status().isOk());
+    }
+
+    @Test
+    // quando chamar o findById do ProductResource passando um id existente deveria retornar um Product
+    public void findByIdShouldReturnProductWhenIdExists() throws Exception {
+
+        // Fazemos a chamar na url /products/{id} passando um id existente
+        ResultActions result = mockMvc.perform(get("/products/{id}", existingId)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // Verificar se retornou um status ok
+        result.andExpect(status().isOk());
+        // Verificar no corpo da resposta json se existe os campos
+        result.andExpect(jsonPath("$.id").exists());
+        result.andExpect(jsonPath("$.name").exists());
+        result.andExpect(jsonPath("$.description").exists());
+    }
+
+    @Test
+    // quando chamar o findById do ProductResource passando um id inexistente deveria retornar um NotFound
+    public void findByIdShouldReturnNotFoundWhenIdDoesNotExists() throws Exception {
+
+        // Fazemos a chamar na url /products/{id} passando um id inexistente
+        ResultActions result = mockMvc.perform(get("/products/{id}", nonExistingId)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // Verificar se retornou um status NotFound
+        result.andExpect(status().isNotFound());
     }
 
 }
